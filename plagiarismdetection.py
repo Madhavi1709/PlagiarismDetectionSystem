9619d463d8b01db93f7a708f1152d8abb54e9c10
Madhavi1709 <madhavi17r@gmail.com>
2026-01-07 11:37:30 +0530

    Update plagiarismdetection.py

--- plagiarismdetection.py
+++ plagiarismdetection.py
@@ -32,6 +32,10 @@ def index():
         similarity_score=similarity_score,
         result=result
     )
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
